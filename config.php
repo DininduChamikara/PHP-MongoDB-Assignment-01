6772307f6857c1107d3f0cd63c335a5e501f2ebb
dinindu99 <peiristd-im18057@stu.kln.ac.lk>
2022-08-29 23:22:31 +0530

    Data was shown as a table

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 
 
     $client = new MongoDB\Client('mongodb://localhost:27017') or die("Error: Cannot create object");
-    echo "Connection to database successfully";
+    // echo "Connection to database successfully";
 ?>
--- index.php
+++ index.php
@@ -9,9 +9,12 @@ include 'config.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 
 <body>
+
+
     <h1>Test Heading</h1>
     <?php
     if (file_exists('cd_catalog.xml')) {
@@ -44,16 +47,60 @@ include 'config.php';
     <br>
 
     <?php
-    $cd_albums = $albums->find([]);
+    // $cd_albums = $albums->find([]);
 
-    foreach ($cd_albums as $cd_album) {
-        echo $cd_album['title'];
-    ?>
-        <br>
-    <?php
-    }
+    // foreach ($cd_albums as $cd_album) {
+    //     echo $cd_album['title'];
+    // ?>
+    //     <br>
+    // <?php
+    // }
 
     ?>
+
+
+
+
+    <br><br><br>
+
+    <div class="heading">
+        <h1>All the times favourite</h1>
+        <h1>Music Albums</h1>
+    </div>
+
+    <a class="btn btn-primary" style="float:right" href="">Add New Album</a>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Country</th>
+                <th>Company</th>
+                <th>Price</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $cd_albums = $albums->find([]);
+            foreach ($cd_albums as $cd_album) {
+            ?>
+                <tr>
+                    <td><?php echo $cd_album['title'] . " (" . $cd_album['year'] . ")" ?></td>
+                    <td><?php echo $cd_album['artist'] ?></td>
+                    <td><?php echo $cd_album['country'] ?></td>
+                    <td><?php echo $cd_album['company'] ?></td>
+                    <td><?php echo "$ " . $cd_album['price'] ?></td>
+                    <td><a class="btn btn-info" href="#">Edit</a>&nbsp;&nbsp; <a class="btn btn-danger" href="#">Delete</a></td>
+                </tr>
+
+            <?php
+            }
+            ?>
+        </tbody>
+
+    </table>
 
 </body>
 
