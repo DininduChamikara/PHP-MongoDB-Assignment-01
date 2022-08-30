20950fd7f175d7c55846e7f326624e52be94168a
dinindu99 <peiristd-im18057@stu.kln.ac.lk>
2022-08-31 00:24:25 +0530

    Upto show list toggle

--- index.php
+++ index.php
@@ -10,26 +10,50 @@ include 'config.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <script>
+        function loadAlbums() {
+
+            if (document.getElementById("demo").style.display == 'none') {
+                document.getElementById("demo").style.display = 'block';
+                document.getElementById('loadAlbumsBtn').innerText = 'Close Albums List'
+            } else {
+                document.getElementById("demo").style.display = 'none';
+                document.getElementById('loadAlbumsBtn').innerText = 'Show Albums List'
+            }
+
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("demo").innerHTML =
+                        this.responseText;
+                }
+            };
+            xhttp.open("GET", "getAlbums.php", true);
+            xhttp.send();
+
+        }
+    </script>
+
+
 </head>
 
 <body>
 
-
-    <h1>Test Heading</h1>
     <?php
     if (file_exists('cd_catalog.xml')) {
         $xml = simplexml_load_file('cd_catalog.xml');
-        echo 'Successfully loaded';
+        // echo 'Successfully loaded';
     } else {
         exit('Failed to open cd_catalog.xml.');
     }
     // $client was defined in the config.php
     $albums = $client->selectCollection('WebAssignmentDB_test_1', 'cd_catalog_data');
-    echo "Collection albums from database WebAssignmentDB_test_1 selected";
+    // echo "Collection albums from database WebAssignmentDB_test_1 selected";
 
     // Delete predefined data
     $deleteResult = $albums->deleteMany([]);
-    printf("Deleted %d document(s)\n", $deleteResult->getDeletedCount());
+    // printf("Deleted %d document(s)\n", $deleteResult->getDeletedCount());
 
     // insert data to the mongoDB 
     foreach ($xml->CD as $cd) {
@@ -44,63 +68,23 @@ include 'config.php';
     }
     ?>
 
-    <br>
 
-    <?php
-    // $cd_albums = $albums->find([]);
-
-    // foreach ($cd_albums as $cd_album) {
-    //     echo $cd_album['title'];
-    // ?>
-    //     <br>
-    // <?php
-    // }
-
-    ?>
-
-
-
-
-    <br><br><br>
 
     <div class="heading">
         <h1>All the times favourite</h1>
         <h1>Music Albums</h1>
     </div>
 
+    <button id="loadAlbumsBtn" type="button" onclick="loadAlbums()">Show Albums List</button>
+    <br><br>
+
+    <span id="demo"></span>
+
+
+
     <a class="btn btn-primary" style="float:right" href="">Add New Album</a>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Title</th>
-                <th>Artist</th>
-                <th>Country</th>
-                <th>Company</th>
-                <th>Price</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $cd_albums = $albums->find([]);
-            foreach ($cd_albums as $cd_album) {
-            ?>
-                <tr>
-                    <td><?php echo $cd_album['title'] . " (" . $cd_album['year'] . ")" ?></td>
-                    <td><?php echo $cd_album['artist'] ?></td>
-                    <td><?php echo $cd_album['country'] ?></td>
-                    <td><?php echo $cd_album['company'] ?></td>
-                    <td><?php echo "$ " . $cd_album['price'] ?></td>
-                    <td><a class="btn btn-info" href="#">Edit</a>&nbsp;&nbsp; <a class="btn btn-danger" href="#">Delete</a></td>
-                </tr>
 
-            <?php
-            }
-            ?>
-        </tbody>
-
-    </table>
 
 </body>
 
